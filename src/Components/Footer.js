05c4d95e8cd426ddf6c2964bf66e97d208b398b9
Eshita Talukdar <146707453+eshitatalukdar08@users.noreply.github.com>
2025-08-29 20:53:27 +0530

    Delete src directory

--- src/Components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer({length}){
-    const today=new Date();
-    return(
-       <footer>
-        <p>{length} Number of {length === 1  ? "Item" : "Items"}</p>
-       </footer>
-    );
-}
--- src/Components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header({title = "Default Title"}){
-    return(
-       <header>
-        <h1>{title}</h1>
-       </header>
-    );
-}
