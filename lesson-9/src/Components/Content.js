fad388a4b165f7fe9aee9b5506fe889e9b576ce9
Eshita Talukdar <146707453+eshitatalukdar08@users.noreply.github.com>
2025-08-30 11:20:34 +0530

    Add files via upload

---
+++ lesson-9/src/Components/Content.js
@@ -0,0 +1,19 @@
+import ItemList from './ItemList.js';
+
+export default function Content({items, handleCheck, handleDelete}){
+/*     ----LESSON 9----      */
+
+    return(
+      <main>
+      {items.length ? (
+       <ItemList 
+       items={items}  
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+       />
+      ) : (
+        <p style={{marginTop: '6rem'}}>Your list is empty</p>
+      )}
+      </main>
+    );
+}
